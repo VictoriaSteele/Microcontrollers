8cd91cb46a44f57650fc0a9edfdbac4ac6f797b4
VictoriaSteele <Victoriae.steele88@gmail.com>
2014-10-25 19:54:37 -0400

    Reset watcher for real

---
+++ project_4/Drivers/Reset_Watcher.c
@@ -0,0 +1,11 @@
+/*!
+ * @file
+ * @brief Module to watch the Rx210 reset conditions
+ *
+ * Copyright (c) 2014 - General Electric - All rights reserved.
+ */
+
+char *Reset_Watcher_GetConditions(void)
+{
+
+}
